1b9fa5796cf5393a2cfb2a6f369072249916683a
Mario1902 <dimitrievskimario@gmail.com>
2024-01-23 11:10:00 +0100

    Proekt3

---
+++ src/Car/Car.java
@@ -0,0 +1,44 @@
+package Car;
+
+public class Car {
+	    private String make;
+	    private String model;
+	    private int year;
+
+	    public Car() {
+	        this.make = "Unknown";
+	        this.model = "Unknown";
+	        this.year = 2023;
+	    }
+
+	    public Car(String make, String model, int year) {
+	        this.make = make;
+	        this.model = model;
+	        this.year = year;
+	    }
+
+	    public String getMake() {
+	        return make;
+	    }
+
+	    public String getModel() {
+	        return model;
+	    }
+
+	    public int getYear() {
+	        return year;
+	    }
+
+	    public void setMake(String make) {
+	        this.make = make;
+	    }
+
+	    public void setModel(String model) {
+	        this.model = model;
+	    }
+
+	    public void setYear(int year) {
+	        this.year = year;
+	    }
+	}
+
---
+++ src/Car/car1.java
@@ -0,0 +1,12 @@
+package Car;
+
+public class car1 {
+
+	public static void main(String[] args) {
+		 Car myCar = new Car("Toyota", "Camry", 2022);
+	        System.out.println("Make: " + myCar.getMake());
+	        System.out.println("Model: " + myCar.getModel());
+	        System.out.println("Year: " + myCar.getYear());
+	}
+
+}
